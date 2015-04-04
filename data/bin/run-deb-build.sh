a26b88815f74d8a41d3eb1134b55fd6f261894e9
euforia <euforia@gmail.com>
2015-04-04 16:46:31 -0700

    Update: build scripts

--- data/bin/run-deb-build.sh
+++ data/bin/run-deb-build.sh
@@ -16,18 +16,19 @@ su - $BUILD_USER -c "cp -a $REPO_LOCAL_PATH/$PKG_DISTRO/debian/control.tar.gz ~/
 if [ "$BUILD_TYPE" == "source" ]; then
     # Build source.
     if [ "$BUILD_CMD" != "" ]; then
-        fire_build_event "build:started" "$PROJECT"
+        fire_build_event "build:started" "$BUILD_USER/$PROJECT"
         # User build command
         su - $BUILD_USER -c "cd $PROJECT_PATH && $BUILD_CMD" || {
-            fire_event_exit "build:failed" "$PROJECT" 3;
-        }
-        # TODO: fire - user-build-success event    
-        fire_build_event "build:succeeded" "$PROJECT";
+            fire_event_exit "build:failed" "$BUILD_USER/$PROJECT" 3;
+        }   
 
         copy_startup "$PROJECT_PATH/build/$PROJECT";
 
         # Create data tarball
-        su - $BUILD_USER -c "cd ~/debuild && tar zcvf data.tar.gz -C $PROJECT_PATH/build/$PROJECT ." || exit 4; 
+        su - $BUILD_USER -c "cd ~/debuild && tar zcvf data.tar.gz -C $PROJECT_PATH/build/$PROJECT ." || {
+            fire_event_exit "build:failed" "$BUILD_USER/$PROJECT" 4; 
+        }
+        fire_build_event "build:succeeded" "$BUILD_USER/$PROJECT";
     else
         echo " ** WARNING: No build command specified! **"
     fi
@@ -35,16 +36,18 @@ else
     # Binary (pre-compiled)
     copy_startup "$PROJECT_PATH"
 
-    su - $BUILD_USER -c "cd ~/debuild && tar czvf data.tar.gz -C $PROJECT_PATH ." || exit 3;
+    su - $BUILD_USER -c "cd ~/debuild && tar czvf data.tar.gz -C $PROJECT_PATH ." || {
+        fire_build_event "build:failed" "$BUILD_USER/$PROJECT" 3;
+    }
 fi
 
-fire_build_event "package:deb:started" "$PROJECT"
+fire_build_event "package:deb:started" "$BUILD_USER/$PROJECT"
 # Make .deb (i.e. ar -r ...)
 su - $BUILD_USER -c "cd ~/debuild && ar -r ${PROJECT}_${PKG_VERSION}-${PKG_RELEASE}_amd64.deb debian-binary control.tar.gz data.tar.gz" || {
-    fire_event_exit "package:deb:failed" "$PROJECT" 5;
+    fire_event_exit "package:deb:failed" "$BUILD_USER/$PROJECT" 5;
 }
 
-fire_build_event "package:deb:succeeded" "$PROJECT"
+fire_build_event "package:deb:succeeded" "$BUILD_USER/$PROJECT"
 
 # Copy .deb back to repo
 add_pkg_to_repo "$BUILD_HOME_DIR/debuild/"
--- data/bin/run-rpm-build.sh
+++ data/bin/run-rpm-build.sh
@@ -10,51 +10,58 @@ install_deps "yum"
 
 # Copy spec file from repo
 su - $BUILD_USER -c "cp $REPO_LOCAL_PATH/$PKG_DISTRO/$PROJECT.spec ~/rpmbuild/SPECS/" || {
-    fire_event_exit "build:failed" "Could not copy: $PROJECT.spec" 2;
+    #fire_event_exit "build:failed" "Could not copy: $PROJECT.spec" 2;
+    exit 2;
 }
 
 if [ "$BUILD_TYPE" == "source" ]; then
     # Build source.
     if [ "$BUILD_CMD" != "" ]; then
-        fire_build_event "build:started" "$PROJECT : $BUILD_CMD"
+        fire_build_event "build:started" "$BUILD_USER/$PROJECT"
         
         su - $BUILD_USER -c "cd $PROJECT_PATH && $BUILD_CMD" || {
-            fire_event_exit "build:failed" "Build command failed: $BUILD_CMD" 3
+            #fire_event_exit "build:failed" "Build command failed: $BUILD_CMD" 3
+            fire_event_exit "build:failed" "$BUILD_USER/$PROJECT" 3;
         }
         # Copy package data to rpm SOURCES destination
         su - $BUILD_USER -c "cp -a $PROJECT_PATH/build/$PROJECT ~/rpmbuild/SOURCES/" || {
-            fire_event_exit "build:failed" "Project build not found in: ./build/$PROJECT" 4
+            #fire_event_exit "build:failed" "Project build not found in: ./build/$PROJECT" 4
+            fire_event_exit "build:failed" "$BUILD_USER/$PROJECT" 4;
         }
         copy_startup "$BUILD_HOME_DIR/rpmbuild/SOURCES/$PROJECT"
         
         # Write file list to spec when being built.
         su - $BUILD_USER -c "cd ~/rpmbuild/SOURCES/$PROJECT && ( find . -type f | sed s/^\.//g >> ~/rpmbuild/SPECS/$PROJECT.spec ) && cd -" || {
-            fire_event_exit "build:failed" "Could not write rpm file list!" 5
+            #fire_event_exit "build:failed" "Could not write rpm file list!" 5
+            fire_event_exit "build:failed" "$BUILD_USER/$PROJECT" 5
         }
         # Copy updated spec back to repo after file list update.
         cp $BUILD_HOME_DIR/rpmbuild/SPECS/$PROJECT.spec $REPO_LOCAL_PATH/$PKG_DISTRO/
 
-        fire_build_event "build:succeeded" "$PROJECT : $BUILD_CMD"
+        fire_build_event "build:succeeded" "$BUILD_USER/$PROJECT"
+
     else
         echo " ** No build command specified! **"
     fi
 else
   # Binary
   copy_startup "$BUILD_HOME_DIR/rpmbuild/SOURCES/$PROJECT"
-  
-  su - $BUILD_USER -c "cp -a $PROJECT_PATH $BUILD_HOME_DIR/rpmbuild/SOURCES/";
+  su - $BUILD_USER -c "cp -a $PROJECT_PATH $BUILD_HOME_DIR/rpmbuild/SOURCES/" || {
+    echo "Could not copy binary files!"
+    exit 3;
+  }
 fi
 
-fire_build_event "package:rpm:started" "$PROJECT";
+fire_build_event "package:rpm:started" "$BUILD_USER/$PROJECT";
 #
 # Build rpm from spec
 # QA_RPATHS=$[ 0x0001|0x0010 ] : Ignore check-rpath warning 
 #
 su - $BUILD_USER -c "QA_RPATHS=$[ 0x0001|0x0010 ] rpmbuild -ba ~/rpmbuild/SPECS/$PROJECT.spec" || {
-    fire_event_exit "package:rpm:failed" "$PROJECT" 6;
+    fire_event_exit "package:rpm:failed" "$BUILD_USER/$PROJECT" 6;
 }
 
-fire_build_event "package:rpm:succeeded" "RPM built."
+fire_build_event "package:rpm:succeeded" "$BUILD_USER/$PROJECT"
 
 # Copy RPM back to repo
 add_pkg_to_repo "$BUILD_HOME_DIR/rpmbuild/RPMS/"
